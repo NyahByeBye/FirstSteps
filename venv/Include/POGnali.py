6bee4a40a6ebfc26b847f1fa8ba906485d508f32
Максим Неведров <44967729+NyahByeBye@users.noreply.github.com>
2020-11-22 21:57:51 +0300

    Nudavai

---
+++ venv/Include/POGnali.py
@@ -0,0 +1,9 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+i=0
+while a:
+    if a[i]<5:
+        print (a[i])
+        i+=1
+
+
+#https://tproger.ru/problems/python-3-exercises-for-beginners-geekbrains/
--- venv/Include/Test.py
+++ venv/Include/Test.py
@@ -1,3 +1,15 @@
-import time
+import random
+import matplotlib
 
-print('Hello world')
+#Закипит ли вода
+
+temp = 20
+heat = 0
+time = 0
+while temp<100:
+    heat=random.paretovariate(100000000)
+    print(heat)
+    temp+=heat
+    time+=1
+    heat=0
+print("Температура: " + str(temp) + ", время: " + str(time//60) + " минут, " + str(time%60) + " секунд")
